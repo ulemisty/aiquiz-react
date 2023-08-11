d286ff14c503a61d4d0bcbd5823d75edef960ee0
yegor <ulemisty@gmail.com>
2023-08-11 21:54:16 +0300

    Updated Environment Variable usage

--- src/openaiClient.js
+++ src/openaiClient.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const config = new Configuration({
-  apiKey: import.meta.env.OPENAI_API_KEY,
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(config);
