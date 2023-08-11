3a75b793265728ebafe97945aa547a7b7e93e85f
yegor <ulemisty@gmail.com>
2023-08-11 21:22:09 +0300

    edited import of env variable

--- src/openaiClient.js
+++ src/openaiClient.js
@@ -1,7 +1,7 @@
 import { Configuration, OpenAIApi } from "openai";
 
 const config = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: import.meta.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(config);
